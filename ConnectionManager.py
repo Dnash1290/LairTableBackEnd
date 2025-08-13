145273bb5fb94859057519dadf8f8ec0bb432dcd
Dnash1290 <nashestev@gmail.com>
2025-08-13 15:07:00 +0100

    handling basic disconnects and connections

--- ConnectionManager.py
+++ ConnectionManager.py
@@ -5,18 +5,22 @@ class ConnetionManager:
         self.connections_dict: dict[str:WebSocket] = {}
         self.rooms = ["testRoom"]
 
-    async def connect(self, client_id:str, room_id, ebsocket: WebSocket):
+    async def connect(self, client_id:str, room_id,websocket: WebSocket):
         await websocket.accept()
         if room_id not in self.rooms:
-            await websocket.close(code=200, reason=f"{room_id} room is invalid" )
-            return f"{room_id} room is invalid"
+            await websocket.close(code=1001, reason=f"{room_id} room is invalid" )
+            return {"success":False, "message":f"{room_id} room is invalid"}
         
         if client_id not in self.connections_dict:
             self.connections_dict[client_id] = websocket       
-            return f"{client_id} has joined"
+            return {"success":True, "message":f"{client_id} has joined"}
+
+        await websocket.close(code=1001, reason=f"{client_id} already has joined in" )
+        return {"success":False, "message":f"{client_id} already has joined in"}
         
     def disconnect(self, client_id):
         self.connections_dict.pop(client_id)
+        print(self.connections_dict, "fkidps")
 
     async def echo_all(self, msg):
         for client in self.connections_dict.values():
--- Routes/UserConnections.py
+++ Routes/UserConnections.py
@@ -5,13 +5,21 @@ conn_router = APIRouter()
 
 @conn_router.websocket("/ws/{room_id}/{client_id}")
 async def connect_client(websocket: WebSocket, room_id, client_id):
+   
+    join = await conn.connect(client_id, room_id, websocket)
+    print(join["message"])
+
+    if not join["success"]:return
+
     try:
-        join = await conn.connect(client_id, room_id, websocket)
-        await conn.echo_all(join)
-        msg = await websocket.receive_json()
-        await websocket.send_text(msg["message"])
-    
+        while True:
+            msg = await websocket.receive_json()
+            await conn.echo_all(str(join))
+            await websocket.send_text(msg["message"])
+            print(websocket.client_state)
+
     except WebSocketDisconnect:
         conn.disconnect(client_id)
-        print("client disconnected")
+        print("left")
+
 
